bcbea96319c024f7d5dbf84d63ea9655a5bbf4cb
Evans Shango <eshango@btn-solutions.co.ke>
2022-04-04 03:28:07 +0300

    fix db connection string for heroku db

--- Startup.cs
+++ Startup.cs
@@ -35,31 +35,28 @@ public class Startup {
 
         services.AddDbContext<StoreContext>(opt => {
             var env = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
-            var connStr = string.Empty;
+            string connStr;
             if (env == "Development") {
                 // Use connection string from file
                 connStr = _config.GetConnectionString("DefaultConnection");
             }
             else {
                 // Use connection string provided at runtime by heroku
-                var connUrl = Environment.GetEnvironmentVariable("DATABSE_URL");
-                if (connUrl != null) {
-                    // Parse connection URL to connection string for NpgSql
-                    connUrl = connUrl.Replace("postgres://", string.Empty);
-                    var pgUserPass = connUrl.Split("@")[0];
-                    var pgHostPortDb = connUrl.Split("@")[1];
-                    var pgHostPort = pgHostPortDb.Split("/")[0];
-                    var pgDb = pgHostPortDb.Split("/")[1];
-                    var pgUser = pgUserPass.Split(":")[0];
-                    var pgPass = pgUserPass.Split(":")[1];
-                    var pgHost = pgHostPort.Split(":")[0];
-                    var pgPort = pgHostPort.Split(":")[1];
+                var connUrl = Environment.GetEnvironmentVariable("DATABASE_URL");
+                // Parse connection URL to connection string for NpgSql
+                connUrl = connUrl?.Replace("postgres://", string.Empty);
+                var pgUserPass = connUrl?.Split("@")[0];
+                var pgHostPortDb = connUrl?.Split("@")[1];
+                var pgHostPort = pgHostPortDb?.Split("/")[0];
+                var pgDb = pgHostPortDb?.Split("/")[1];
+                var pgUser = pgUserPass?.Split(":")[0];
+                var pgPass = pgUserPass?.Split(":")[1];
+                var pgHost = pgHostPort?.Split(":")[0];
+                var pgPort = pgHostPort?.Split(":")[1];
 
-                    connStr =
-                        $"Server={pgHost};Port={pgPort};User Id={pgUser};Password={pgPass};Database={pgDb};SSL Mode=Require;Trust Server Certificate=true";
-                }
+                connStr =
+                    $"Server={pgHost};Port={pgPort};User Id={pgUser};Password={pgPass};Database={pgDb};SSL Mode=Require;Trust Server Certificate=true";
             }
-
             opt.UseNpgsql(connStr);
         });
 
