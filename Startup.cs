b0bf1e939f4dcbf577f7c2113f7ee8b4a1464b90
Evans Shango <eshango@btn-solutions.co.ke>
2022-04-04 03:08:50 +0300

    fix db connection string for heroku db

--- Startup.cs
+++ Startup.cs
@@ -44,15 +44,15 @@ public class Startup {
                 // Use connection string provided at runtime by heroku
                 var connUrl = Environment.GetEnvironmentVariable("DATABSE_URL");
                 // Parse connection URL to connection string for NpgSql
-                connUrl = connUrl.Replace("postgres://", string.Empty);
-                var pgUserPass = connUrl.Split("@")[0];
-                var pgHostPortDb = connUrl.Split("@")[1];
-                var pgHostPort = pgHostPortDb.Split("/")[0];
-                var pgDb = pgHostPortDb.Split("/")[1];
-                var pgUser = pgUserPass.Split(":")[0];
-                var pgPass = pgUserPass.Split(":")[1];
-                var pgHost = pgHostPort.Split(":")[0];
-                var pgPort = pgHostPort.Split(":")[1];
+                connUrl = connUrl?.Replace("postgres://", string.Empty);
+                var pgUserPass = connUrl?.Split("@")[0];
+                var pgHostPortDb = connUrl?.Split("@")[1];
+                var pgHostPort = pgHostPortDb?.Split("/")[0];
+                var pgDb = pgHostPortDb?.Split("/")[1];
+                var pgUser = pgUserPass?.Split(":")[0];
+                var pgPass = pgUserPass?.Split(":")[1];
+                var pgHost = pgHostPort?.Split(":")[0];
+                var pgPort = pgHostPort?.Split(":")[1];
                 
                 connStr = $"Server={pgHost};Port={pgPort};User Id={pgUser};Password={pgPass};Database={pgDb};SSL Mode=Require;Trust Server Certificate=true";
             }
